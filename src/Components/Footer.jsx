5613045c3134bb3aede79c5915ef67a302848271
agarwalSuyash <suyumr@gmail.com>
2021-08-02 16:02:26 +0530

    Creating the basic layout of the keepr app

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+    const d = new Date();
+    return (<footer>
+    <p>Copyright {d.getFullYear()}</p>
+    </footer>);
+}
+
+export default Footer;
